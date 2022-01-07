ed02cd6521dce5234de0d8558675f9a2d66a8c41
Jamie Tanna <jamie@jamietanna.co.uk>
2022-01-07 14:53:05 +0000

    fixup! Add a rule to enforce semantic versioning
    It appears the CI checks hadn't executed, so this commit wasn't spotted
    to be broken.

--- spectral-ruleset-govuk-public/test/semver.test.js
+++ spectral-ruleset-govuk-public/test/semver.test.js
@@ -1,40 +1,40 @@
-const { retrieveDocument, setupSpectral, getErrors, resultsForCode } = require('@jamietanna/spectral-test-harness')
+const { retrieveDocument, setupSpectral, resultsForCode } = require('@jamietanna/spectral-test-harness')
 
 describe('semver', () => {
-    test('fails when not a number', async () => {
-        const spectral = await setupSpectral('ruleset.yaml')
-        const document = retrieveDocument('semver/invalid-letters.yaml')
+  test('fails when not a number', async () => {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument('semver/invalid-letters.yaml')
 
-        const results = resultsForCode(await spectral.run(document), 'semver')
+    const results = resultsForCode(await spectral.run(document), 'semver')
 
-        expect(results).toHaveLength(1)
-        expect(results[0]['message']).toEqual('Version should use semantic versioning. abc is not a valid version.')
-    })
+    expect(results).toHaveLength(1)
+    expect(results[0].message).toEqual('Version should use semantic versioning. abc is not a valid version.')
+  })
 
-    test('fails when empty', async () => {
-        const spectral = await setupSpectral('ruleset.yaml')
-        const document = retrieveDocument('semver/invalid-empty.yaml')
+  test('fails when empty', async () => {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument('semver/invalid-empty.yaml')
 
-        const results = resultsForCode(await spectral.run(document), 'semver')
+    const results = resultsForCode(await spectral.run(document), 'semver')
 
-        expect(results).toHaveLength(1)
-    })
+    expect(results).toHaveLength(1)
+  })
 
-    test('fails when there is a major and minor version but no patch', async () => {
-        const spectral = await setupSpectral('ruleset.yaml')
-        const document = retrieveDocument('semver/invalid-no-patch.yaml')
+  test('fails when there is a major and minor version but no patch', async () => {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument('semver/invalid-no-patch.yaml')
 
-        const results = resultsForCode(await spectral.run(document), 'semver')
+    const results = resultsForCode(await spectral.run(document), 'semver')
 
-        expect(results).toHaveLength(1)
-    })
+    expect(results).toHaveLength(1)
+  })
 
-    test('passes when numbers have multiple digits', async () => {
-        const spectral = await setupSpectral('ruleset.yaml')
-        const document = retrieveDocument('semver/valid-complex-number.yaml')
+  test('passes when numbers have multiple digits', async () => {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument('semver/valid-complex-number.yaml')
 
-        const results = resultsForCode(await spectral.run(document), 'semver')
+    const results = resultsForCode(await spectral.run(document), 'semver')
 
-        expect(results).toHaveLength(0)
-    })
+    expect(results).toHaveLength(0)
+  })
 })
