fd8c6bf11ebfa3e5c838c2c2af884cc81bb2cb09
Jamie Tanna <jamie@jamietanna.co.uk>
2022-03-08 13:34:45 +0000

    Add rule for requiring HTTP 415 on all resources
    As noted in #35, we should require that these status codes are present,
    as it makes it clearer to the consumer that they are expected.
    
    Although producers of APIs may not think they are relevant, as they
    could be handled by the web framework, we should make it clear that this
    is still required.

---
+++ spectral-ruleset-govuk-public/test/unsupported-media-type.test.js
@@ -0,0 +1,47 @@
+const { retrieveDocument, setupSpectral, resultsForCode } = require('@jamietanna/spectral-test-harness')
+
+describe('unsupported-media-type', () => {
+  async function isNotRequired (method) {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument(`unsupported-media-type/absent-${method}.yaml`)
+
+    const results = resultsForCode(await spectral.run(document), 'unsupported-media-type')
+
+    expect(results).toHaveLength(0)
+  }
+
+  async function isRequired (method) {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument(`unsupported-media-type/absent-${method}.yaml`)
+
+    const results = resultsForCode(await spectral.run(document), 'unsupported-media-type')
+
+    expect(results).toHaveLength(1)
+    expect(results[0].message).toEqual('Resources that consume a request body must declare a response for an HTTP 415 Unsupported Media Type')
+  }
+
+  describe('fails when 415 is not present', () => {
+    ['PUT', 'POST', 'PATCH'].forEach(async (method) => {
+      test(`on ${method}`, async () => {
+        await isRequired(method.toLowerCase())
+      })
+    })
+  })
+
+  describe('passes when 415 is not present', () => {
+    ['GET', 'HEAD', 'OPTIONS', 'TRACE'].forEach(async (method) => {
+      test(`on ${method}`, async () => {
+        await isNotRequired(method.toLowerCase())
+      })
+    })
+  })
+
+  test('passes when 415 is present', async () => {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument('unsupported-media-type/valid.yaml')
+
+    const results = resultsForCode(await spectral.run(document), 'unsupported-media-type')
+
+    expect(results).toHaveLength(0)
+  })
+})
