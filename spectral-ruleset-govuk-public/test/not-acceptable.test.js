58ea115b766965e716815aa48fc579132425887c
Jamie Tanna <jamie@jamietanna.co.uk>
2022-03-08 13:34:45 +0000

    Add rule for requiring HTTP 406 on all resources
    As noted in #35, we should require that these status codes are present,
    as it makes it clearer to the consumer that they are expected.
    
    Although producers of APIs may not think they are relevant, as they
    could be handled by the web framework, we should make it clear that this
    is still required.

---
+++ spectral-ruleset-govuk-public/test/not-acceptable.test.js
@@ -0,0 +1,47 @@
+const { retrieveDocument, setupSpectral, resultsForCode } = require('@jamietanna/spectral-test-harness')
+
+describe('not-acceptable', () => {
+  async function isNotRequired (method) {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument(`not-acceptable/absent-${method}.yaml`)
+
+    const results = resultsForCode(await spectral.run(document), 'not-acceptable')
+
+    expect(results).toHaveLength(0)
+  }
+
+  async function isRequired (method) {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument(`not-acceptable/absent-${method}.yaml`)
+
+    const results = resultsForCode(await spectral.run(document), 'not-acceptable')
+
+    expect(results).toHaveLength(1)
+    expect(results[0].message).toEqual('Resources that can provide a response body must declare a response for an HTTP 406 Not Acceptable')
+  }
+
+  describe('fails when 406 is not present', () => {
+    ['GET', 'PUT', 'POST', 'PATCH'].forEach(async (method) => {
+      test(`on ${method}`, async () => {
+        await isRequired(method.toLowerCase())
+      })
+    })
+  })
+
+  describe('passes when 406 is not present', () => {
+    ['HEAD', 'OPTIONS', 'TRACE'].forEach(async (method) => {
+      test(`on ${method}`, async () => {
+        await isNotRequired(method.toLowerCase())
+      })
+    })
+  })
+
+  test('passes when 406 is present', async () => {
+    const spectral = await setupSpectral('ruleset.yaml')
+    const document = retrieveDocument('not-acceptable/valid.yaml')
+
+    const results = resultsForCode(await spectral.run(document), 'not-acceptable')
+
+    expect(results).toHaveLength(0)
+  })
+})
