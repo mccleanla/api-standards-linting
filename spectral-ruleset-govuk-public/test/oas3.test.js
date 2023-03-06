1272b244d29cf26184c6fbbad6aedb630a22fa39
Bethan <bethan.palmer@digital.cabinet-office.gov.uk>
2023-03-06 11:35:43 +0000

    Add the strictest ruleset
    This contains the most opinionated rules of the three rulesets.  A couple of
    rules (unsupported media type and not acceptable) have been moved into this one
    as these aren't always essential and should be in the stricter ruleset.
    
    Some of the rules add to the ones in the medium ruleset, for example the
    license rule and oas version rule are more specific.

--- spectral-ruleset-govuk-public/test/oas3.test.js
+++ spectral-ruleset-govuk-public/test/oas3.test.js
@@ -28,4 +28,14 @@ describe('oas3', () => {
 
     expect(results).toHaveLength(0)
   })
+
+  test('oas3-strict fails when oas version is 3.0.0', async () => {
+    const spectral = await setupSpectral('ruleset-high.yaml')
+    const document = retrieveDocument('oas3/3.0.0.yaml')
+
+    const results = resultsForCode(await spectral.run(document), 'oas3-strict')
+
+    expect(results).toHaveLength(1)
+    expect(results[0].message).toEqual('OpenAPI version must be 3.1.0')
+  })
 })
